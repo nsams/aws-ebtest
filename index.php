a578e6beb0ec53795bf1b9c508d5e68d97a6acfc
Niko Sams <niko.sams@gmail.com>
2015-02-12 20:40:37 +0100

    add jquery, can't have a website witout

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
  <head>
   <title>PHP Test</title>
+  <script type="text/javascript" src="vendor/bower_components/jquery/dist/jquery.min.js"></script>
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
